710b9558f35ee8b8c69622b75161f537391fb4c0
EduLz <edulpzavila@gmail.com>
2026-07-28 14:08:21 -0600

    chore: update application ID to com.simple.drivesync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     namespace = "com.example.drivesync"
     compileSdk = 36
     defaultConfig {
-        applicationId = "com.edulp.simpledrivesync"
+        applicationId = "com.simple.drivesync"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
